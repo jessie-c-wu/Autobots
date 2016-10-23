7e196896181fc508b7a475e90074222f8d80f452
Darwin Huang <huangdarwin2@gmail.com>
2016-10-23 16:20:23 -0400

    Ultrasonic Code
    Working detection of objects in front, left, right. Threshold set to 5cm < x < 10cm
    
    NOTE: Depends on 5cm stopping distance!!! Will expect 10cm "personal space"

---
+++ Ultrasonic/Ultrasonic.ino
@@ -0,0 +1,78 @@
+#include "Ultrasonic.h"
+
+int ultrasonicLowThreshold = 5;
+int ultrasonicHighThreshold = 10;
+
+Ultrasonic ultrasonic1(4,5);
+Ultrasonic ultrasonic2(6,7);
+Ultrasonic ultrasonic3(8,9);
+Ultrasonic ultrasonic4(10,11);
+Ultrasonic ultrasonic5(12,13);
+
+void setup() {
+Serial.begin(9600);
+}
+
+bool ultrasonicWithinThreshold(Ultrasonic ultrasonic){
+  int read = ultrasonic.Ranging(CM);
+  if(read > ultrasonicLowThreshold && read < ultrasonicHighThreshold){
+    return true;
+  }
+  return false;
+}
+
+bool ultrasonicFront(){
+  return ultrasonicWithinThreshold(ultrasonic2) || ultrasonicWithinThreshold(ultrasonic3) || ultrasonicWithinThreshold(ultrasonic4);
+}
+
+bool ultrasonicLeft(){
+  return ultrasonicWithinThreshold(ultrasonic1);
+}
+
+bool ultrasonicRight(){
+  return ultrasonicWithinThreshold(ultrasonic5);
+}
+
+void printAllRawUltrasonicValues(){
+  Serial.print(ultrasonic1.Ranging(CM));
+  Serial.print("\t");
+  Serial.print(ultrasonic2.Ranging(CM));
+  Serial.print("\t");
+  Serial.print(ultrasonic3.Ranging(CM));
+  Serial.print("\t");
+  Serial.print(ultrasonic4.Ranging(CM));
+  Serial.print("\t");
+  Serial.print(ultrasonic5.Ranging(CM));
+  Serial.print("\t");
+  Serial.println();
+}
+
+void printUltrasonicThresholds(){
+  int inFront = ultrasonicFront();
+  int inLeft = ultrasonicLeft();
+  int inRight = ultrasonicRight();
+
+  if(inFront){
+     Serial.print("FRONT\t");
+  }
+  if(inLeft){
+    Serial.print("LEFT\t");
+  }
+  if(inRight){
+    Serial.print("RIGHT\t");
+  }
+  if(!inFront && !inLeft && !inRight){
+    Serial.print("ALL GOOD");
+  }
+  Serial.println();
+}
+
+void loop()
+{
+  printUltrasonicThresholds();
+  delay(100);
+}
+
+
+
+
